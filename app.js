b9e34e30e7d4c7a37886ef7becfccce6c8a58ad0
senjaathenia <senjaathenia@gmail.com>
2024-06-19 05:08:51 +0100

    express

--- app.js
+++ app.js
@@ -68,90 +68,90 @@ app.get('/contact', (req, res) => {
         }
     });
 });
-// app.get('/tambah', (req, res) => {
-//     res.render('tambah');
-// });
-// app.post('/addContact', (req, res) => {
-//     const { nama, notelp, email } = req.body;
-//     const filePath = path.join(__dirname, 'contacts.json');
-//     fs.readFile(filePath, 'utf8', (err, data) => {
-//         if (err) {
-//             res.status(500).send('Error reading the contacts file');
-//             return;
-//         }
-//         let contacts = [];
-//         if (data) {
-//             contacts = JSON.parse(data);
-//         }
-//         contacts.push({ nama, notelp, email });
-//         fs.writeFile(filePath, JSON.stringify(contacts, null, 2), (err) => {
-//             if (err) {
-//                 res.status(500).send('Error writing to the contacts file');
-//                 return;
-//             }
-//             res.redirect('/contact');
-//         });
-//     });
-// });
-// app.get('/edit/:nama', (req, res) => {
-//     const contactName = req.params.nama;
-//     readContactsFile((err, contacts) => {
-//         if (err) {
-//             res.status(500).send('Error reading contacts file');
-//             return;
-//         }
-//         const contact = contacts.find(c => c.nama === contactName);
-//         if (!contact) {
-//             res.status(404).send('Contact not found');
-//             return;
-//         }
-//         res.render('edit', { contact: contact });
-//     });
-// });
+app.get('/tambah', (req, res) => {
+    res.render('tambah');
+});
+app.post('/addContact', (req, res) => {
+    const { nama, notelp, email } = req.body;
+    const filePath = path.join(__dirname, 'contacts.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading the contacts file');
+            return;
+        }
+        let contacts = [];
+        if (data) {
+            contacts = JSON.parse(data);
+        }
+        contacts.push({ nama, notelp, email });
+        fs.writeFile(filePath, JSON.stringify(contacts, null, 2), (err) => {
+            if (err) {
+                res.status(500).send('Error writing to the contacts file');
+                return;
+            }
+            res.redirect('/contact');
+        });
+    });
+});
+app.get('/edit/:nama', (req, res) => {
+    const contactName = req.params.nama;
+    readContactsFile((err, contacts) => {
+        if (err) {
+            res.status(500).send('Error reading contacts file');
+            return;
+        }
+        const contact = contacts.find(c => c.nama === contactName);
+        if (!contact) {
+            res.status(404).send('Contact not found');
+            return;
+        }
+        res.render('edit', { contact: contact });
+    });
+});
 
-// // Simpan perubahan pada kontak
-// app.post('/edit/:nama', (req, res) => {
-//     const contactName = req.params.nama;
-//     const { nama, notelp, email } = req.body;
-//     readContactsFile((err, contacts) => {
-//         if (err) {
-//             res.status(500).send('Error reading contacts file');
-//             return;
-//         }
-//         const contactIndex = contacts.findIndex(c => c.nama === contactName);
-//         if (contactIndex === -1) {
-//             res.status(404).send('Contact not found');
-//             return;
-//         }
-//         contacts[contactIndex] = { ...contacts[contactIndex], nama, notelp, email };
-//         writeContactsFile(contacts, (err) => {
-//             if (err) {
-//                 res.status(500).send('Error writing to contacts file');
-//                 return;
-//             }
-//             res.redirect('/contact');
-//         });
-//     });
-// });
-// app.post('/hapus/:nama', (req, res) => {
-//     const contactName = req.params.nama;
-//     console.log(`Menghapus kontak dengan nama: ${contactName}`);  // Log nama kontak yang akan dihapus
+// Simpan perubahan pada kontak
+app.post('/edit/:nama', (req, res) => {
+    const contactName = req.params.nama;
+    const { nama, notelp, email } = req.body;
+    readContactsFile((err, contacts) => {
+        if (err) {
+            res.status(500).send('Error reading contacts file');
+            return;
+        }
+        const contactIndex = contacts.findIndex(c => c.nama === contactName);
+        if (contactIndex === -1) {
+            res.status(404).send('Contact not found');
+            return;
+        }
+        contacts[contactIndex] = { ...contacts[contactIndex], nama, notelp, email };
+        writeContactsFile(contacts, (err) => {
+            if (err) {
+                res.status(500).send('Error writing to contacts file');
+                return;
+            }
+            res.redirect('/contact');
+        });
+    });
+});
+app.post('/hapus/:nama', (req, res) => {
+    const contactName = req.params.nama;
+    console.log(`Menghapus kontak dengan nama: ${contactName}`);  // Log nama kontak yang akan dihapus
 
-//     readContactsFile((err, contacts) => {
-//         if (err) {
-//             res.status(500).send('Error reading contacts file');
-//             return;
-//         }
-//         const updatedContacts = contacts.filter(c => c.nama !== contactName);
-//         writeContactsFile(updatedContacts, (err) => {
-//             if (err) {
-//                 res.status(500).send('Error writing to contacts file');
-//                 return;
-//             }
-//             res.redirect('/contact');
-//         });
-//     });
-// });
+    readContactsFile((err, contacts) => {
+        if (err) {
+            res.status(500).send('Error reading contacts file');
+            return;
+        }
+        const updatedContacts = contacts.filter(c => c.nama !== contactName);
+        writeContactsFile(updatedContacts, (err) => {
+            if (err) {
+                res.status(500).send('Error writing to contacts file');
+                return;
+            }
+            res.redirect('/contact');
+        });
+    });
+});
 app.get('/produk/:id', (req, res) => {
     res.send('product id: ' + req.params.id);
 });
